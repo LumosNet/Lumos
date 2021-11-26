b33bf080715b8c793b1614eb68f40cd391707b62
btboay <yuzhuoandlb@foxmail.com>
2021-11-27 01:22:59 +0800

    xxx

--- test/test.c
+++ test/test.c
@@ -3,16 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    int x[] = {1, 0, -1, 1, 0, -1, 1, 0, -1};
+    int x[] = {1, 0, -1};
+    int y[] = {2, 0, -2};
     FILE *fp = fopen("./test.weights", "wb");
-    fwrite(&x, 4, 9, fp);
+    fwrite(&x, 4, 3, fp);
+    fwrite(&y, 4, 3, fp);
     fclose(fp);
 
     fp = fopen("./test.weights", "rb");
-    int *buffer = malloc(9*sizeof(int));
-    fread(buffer, 4, 9, fp);
-    for (int i = 0; i < 9; ++i){
-        printf("%d\n", buffer[i]);
+    int *buffer1 = malloc(12*sizeof(int));
+    // int *buffer2 = malloc(3*sizeof(int));
+    fread(buffer1, 4, 12, fp);
+    // fread(buffer2, 4, 3, fp);
+    for (int i = 0; i < 12; ++i){
+        printf("%d ", buffer1[i]);
     }
+    printf("\n");
     return 0;
 }
+
