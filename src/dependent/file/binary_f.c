cedd52a88eea80889c877ccd359cf74850094c43
libian <yuzhuoandlb@foxmail.com>
2022-05-19 19:46:39 +0800

    binary“

--- src/dependent/file/binary_f.c
+++ src/dependent/file/binary_f.c
@@ -1,33 +1,5 @@
 #include "binary_f.h"
 
-
-// void write_as_binary(FILE *fp, float* array, size_t size)
-// {
-//     fwrite(array, sizeof(float), size, fp);
-// }
-
-
-// void read_part_as_bin(FILE *fp, float**array, size_t *arrsize, size_t length)
-// {
-//     fseek(fp, 0, SEEK_SET);
-//     float *arr = (float*)malloc(sizeof(float)*length);
-//     *array = arr;
-//     int res = fread(arr, sizeof(float), length, fp);
-//     *arrsize = res;
-// }
-
-// void read_all_as_binary(FILE *fp, float**array, size_t *arrsize)
-// {
-//     fseek(fp, 0, SEEK_END);
-//     size_t end = ftell(fp)/sizeof(float);
-//     read_part_as_bin(fp, array, arrsize, end);
-// }
-
-// void read_h2x_as_binary(FILE *fp, size_t length, float**array, size_t *arrsize)
-// {
-//     read_part_as_bin(fp, array, arrsize, length); 
-// }
-
 void bfget(FILE *fp, float *array, size_t size)
 {
     fread(array, sizeof(float), size, fp);
--- src/dependent/file/binary_f.h
+++ src/dependent/file/binary_f.h
@@ -8,12 +8,6 @@
 extern "C" {
 #endif
 
-// void write_as_binary(FILE *fp, float* array, size_t size);
-
-// void read_all_as_binary(FILE *fp, float**array, size_t *arrsize);
-// void read_h2x_as_binary(FILE *fp, size_t length, float**array, \
-//                         size_t *arrsize);
-
 void bfget(FILE *fp, float *array, size_t size);
 void bfput(FILE *fp, float *array, size_t size);
 
