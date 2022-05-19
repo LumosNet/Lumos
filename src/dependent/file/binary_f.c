e9404b81f3af8bb931a859735d7d4fa815e526ee
wangzihao223 <1600964393@qq.com>
2022-05-19 09:38:27 +0800

    二进制读写模块

--- src/dependent/file/binary_f.c
+++ src/dependent/file/binary_f.c
@@ -1,2 +1,86 @@
 #include "binary_f.h"
 
+
+int write_as_binary(FILE *fp, float* arry, size_t size)
+{
+    size_t res = fwrite(arry, sizeof(float), size, fp);
+    if (res != size)
+    {
+        int error = ferror(fp);
+        if (!error)
+        {
+            return FILEEND;
+        }
+        return error;
+    }
+    return SUCESS;
+}
+
+
+int read_part_as_bin(FILE *fp, float**arry, size_t *arrsize, size_t start,\
+     size_t end)
+{
+    int state = fseek(fp, start*sizeof(float), SEEK_SET);
+    if(state)
+    {
+        return FSEEKERROR;
+    }
+    size_t length = end - start;
+    float *arr = (float*)malloc(sizeof(float)*(end-start));
+    *arry = arr;
+    int res = fread(arr, sizeof(float), end-start, fp);
+    
+    if (res != (end-start))
+    {
+        int error = ferror(fp);
+        if (!error)
+        {
+            return FILEEND;
+        }
+        *arrsize = res;
+        return error;
+    }
+    *arrsize = end - start;
+    return SUCESS;
+}
+
+
+
+int read_as_binary(FILE *fp, int mode, size_t *scop, float**arry, size_t *arrsize)
+{
+    int res;
+    size_t end;
+    size_t start;
+    if(!scop && mode != 0)
+    {
+        return ARGSERROR;
+    }
+
+    switch (mode)
+    {
+        case READALL:
+            if(!fseek(fp, 0, SEEK_END))
+            {
+                return FSEEKERROR;
+            }
+            end = ftell(fp)/sizeof(float);
+            return read_part_as_bin(fp, arry, arrsize, 0, end);
+            break;
+        case READPART:
+            start = scop[0];
+            end   = scop[1];
+            if(end<=start)
+            {
+                return RANGEERROR;
+            }
+            return read_part_as_bin(fp, arry, arrsize, start, end);
+            break;
+        case READSERIAL:
+            end  = scop[0];
+            return read_part_as_bin(fp, arry, arrsize, 0, end);
+            break;
+        default:
+            return  MODERROR;
+            break;
+   } 
+}
--- src/dependent/file/binary_f.h
+++ src/dependent/file/binary_f.h
@@ -1,11 +1,42 @@
 #ifndef BINARY_F_H
 #define BINART_F_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*
+** 三种读取模式
+** 0 全读取
+** 1 分段读取
+** 2 部分读取
+*/
+#define READALL     0
+#define READPART    1
+#define READSERIAL  2
 
+/*
+**操作文件信息
+**0 操作成功
+**1-34状态码参考 ferror 返回状态值
+**35 模式错误
+**36 参数错误
+**37 选取范围错误
+**38 fseek操作错误
+*/
+#define SUCESS      0
+#define MODERROR    35
+#define ARGSERROR   36
+#define RANGEERROR  37
+#define FSEEKERROR  38
+#define FILEEND     39
+
+
+int write_as_binary(FILE *fp, float* arry, size_t size);
+int read_as_binary(FILE *fp, int mode, size_t *scop, float**arry, size_t *arrsize);
 
 #ifdef __cplusplus
 }
