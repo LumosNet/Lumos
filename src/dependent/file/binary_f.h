975badf6d419040885b9a90482237f24db69e8cc
libian <yuzhuoandlb@foxmail.com>
2022-05-19 19:43:22 +0800

    binary

--- src/dependent/file/binary_f.h
+++ src/dependent/file/binary_f.h
@@ -8,11 +8,14 @@
 extern "C" {
 #endif
 
-void write_as_binary(FILE *fp, float* array, size_t size);
+// void write_as_binary(FILE *fp, float* array, size_t size);
 
-void read_all_as_binary(FILE *fp, float**array, size_t *arrsize);
-void read_h2x_as_binary(FILE *fp, size_t length, float**array, \
-                        size_t *arrsize);
+// void read_all_as_binary(FILE *fp, float**array, size_t *arrsize);
+// void read_h2x_as_binary(FILE *fp, size_t length, float**array, \
+//                         size_t *arrsize);
+
+void bfget(FILE *fp, float *array, size_t size);
+void bfput(FILE *fp, float *array, size_t size);
 
 #ifdef __cplusplus
 }
