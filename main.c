ac8c9921a97b47b82009f3710d9a6524c3b26292
libian <yuzhuoandlb@foxmail.com>
2022-12-27 07:36:11 +0000

    pooling gpu benchmark

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
 
 int main(void)
 {
-    run_benchmarks("./lumos_t/benchmark/core/ops/pooling/avgpool.json");
-    run_benchmarks("./lumos_t/benchmark/core/ops/pooling/avgpool_gradient.json");
-    run_benchmarks("./lumos_t/benchmark/core/ops/pooling/maxpool.json");
-    run_benchmarks("./lumos_t/benchmark/core/ops/pooling/maxpool_gradient.json");
+    run_benchmarks("./lumos_t/benchmark/core_cu/ops/pooling/avgpool_gpu.json");
+    run_benchmarks("./lumos_t/benchmark/core_cu/ops/pooling/avgpool_gradient_gpu.json");
+    run_benchmarks("./lumos_t/benchmark/core_cu/ops/pooling/maxpool_gpu.json");
+    run_benchmarks("./lumos_t/benchmark/core_cu/ops/pooling/maxpool_gradient_gpu.json");
     return 0;
 }
