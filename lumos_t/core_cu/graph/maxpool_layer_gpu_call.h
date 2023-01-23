64feb17b848ec2e1cf467d0fdb1e57c3dd25b944
libian <yuzhuoandlb@foxmail.com>
2023-01-23 16:38:44 +0800

    graph benchmark

--- lumos_t/core_cu/graph/maxpool_layer_gpu_call.h
+++ lumos_t/core_cu/graph/maxpool_layer_gpu_call.h
@@ -1,3 +1,6 @@
+#ifndef MAXPOOL_LAYER_GPU_CALL_H
+#define MAXPOOL_LAYER_GPU_CALL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,3 +9,19 @@
 #include "device_launch_parameters.h"
 #include "curand.h"
 #include "cublas_v2.h"
+
+#include "layer.h"
+#include "maxpool_layer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void call_forward_maxpool_layer_gpu(void **params, void **ret);
+void call_backward_maxpool_layer_gpu(void **params, void **ret);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
