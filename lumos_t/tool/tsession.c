dda375f04d1e91a5a9a94692b3a39059929283ff
lumos <yuzhuoandlb@foxmail.com>
2023-07-22 12:57:09 +0800

    del tsession

--- lumos_t/tool/tsession.c
+++
@@ -1,9 +0,0 @@
-#include "tsession.h"
-
-SessionT *make_t_session(char *path)
-{
-    SessionT *sess = malloc(sizeof(SessionT));
-    cJSON *CJbenchmark = get_benchmark(path);
-    cJSON *CJpublic = get_public(CJbenchmark);
-    
-}
