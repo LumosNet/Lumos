5a9b298f12d2355d2e3399c6761824cf7276390a
lumos <yuzhuoandlb@foxmail.com>
2023-08-02 00:38:31 +0800

    FlowTest xor

---
+++ lumos_t/flow/xor_call.h
@@ -0,0 +1,23 @@
+#ifndef XOR_CALL_H
+#define XOR_CALL_H
+
+#include "session.h"
+#include "manager.h"
+#include "dispatch.h"
+#include "dispatch.h"
+#include "manager.h"
+#include "session.h"
+#include "graph.h"
+#include "layer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void call_xor(void **params, void **ret);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
