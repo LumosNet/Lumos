3d061a11d9e6a69129644c028998045c3156e64b
libian <yuzhuoandlb@foxmail.com>
2024-05-10 16:03:25 +0800

    demo cifar10

--- lumos/lumos/lumos.c
+++ lumos/lumos/lumos.c
@@ -55,7 +55,7 @@ void mnist(char *type)
     train(sess);
 }
 
-void lenet5(char *type)
+void lenet5_mnist(char *type)
 {
     Graph *g = create_graph();
     Layer *l1 = make_convolutional_layer(6, 5, 1, 0, 1, "relu");
@@ -84,9 +84,38 @@ void lenet5(char *type)
     train(sess);
 }
 
+void lenet5_cifar10(char *type)
+{
+    Graph *g = create_graph();
+    Layer *l1 = make_convolutional_layer(6, 5, 1, 0, 1, "relu");
+    Layer *l2 = make_avgpool_layer(2, 2, 0);
+    Layer *l3 = make_convolutional_layer(16, 5, 1, 0, 1, "relu");
+    Layer *l4 = make_avgpool_layer(2, 2, 0);
+    Layer *l5 = make_convolutional_layer(120, 5, 1, 0, 1, "relu");
+    Layer *l6 = make_im2col_layer();
+    Layer *l7 = make_connect_layer(84, 1, "relu");
+    Layer *l8 = make_connect_layer(10, 1, "relu");
+    Layer *l9 = make_softmax_layer(10);
+    Layer *l10 = make_mse_layer(10);
+    append_layer2grpah(g, l1);
+    append_layer2grpah(g, l2);
+    append_layer2grpah(g, l3);
+    append_layer2grpah(g, l4);
+    append_layer2grpah(g, l5);
+    append_layer2grpah(g, l6);
+    append_layer2grpah(g, l7);
+    append_layer2grpah(g, l8);
+    append_layer2grpah(g, l9);
+    append_layer2grpah(g, l10);
+    Session *sess = create_session(g, 32, 32, 4, 10, type);
+    set_train_params(sess, 15, 16, 16, 0.1);
+    init_session(sess, "./data/cifar10/train.txt", "./data/cifar10/train_label.txt");
+    train(sess);
+}
+
 int main(int argc, char **argv)
 {
     char *type = argv[1];
-    lenet5(type);
+    lenet5_cifar10(type);
     return 0;
 }
